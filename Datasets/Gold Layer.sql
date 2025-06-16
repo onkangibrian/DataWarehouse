13a44fa5abba65c62e6449c8489d9b62986a6c0c
Onkangi <78580327+onkangibrian@users.noreply.github.com>
2025-06-16 09:08:03 +0300

    Create Gold Layer.sql
    The Golden Layer Schemas

---
+++ Datasets/Gold Layer.sql
@@ -0,0 +1,85 @@
+-- Creation of the customers view on the golden layer
+create view gold.dim_customers as -- creation of a view
+select 
+row_number() over (order by cst_id) as customer_key,
+ci.cst_id as customer_id,
+ci.cst_key as customer_number,
+ci.cst_firstname as first_name,
+ci.cst_lastname as last_name,
+la.cntry as country,
+ci.cst_marital_status as marital_status,
+case when ci.cst_gndr != 'n/a' then ci.cst_gndr -- CRM is the Master for gender info
+	 else coalesce(ca.gen,'n/a')
+end as gender,
+ca.bdate as birthdate,
+ci.cst_create_date as create_date
+from silver.crm_cust_info as ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid;
+
+-- checking for anomalies on the gender table on both crm and erp tables
+select distinct 
+	ci.cst_gndr,
+	ca.gen,
+	case when ci.cst_gndr != 'n/a' then ci.cst_gndr
+		 else coalesce(ca.gen,'n/a')
+	end as new_gen
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+order by 1,2
+
+-- ---------------------------------------------------------------------------------------------------
+-- creation of the product view on the golden layer
+create view gold.dim_products as 
+select 
+	row_number() over (order by pn.prd_start_dt,pn.prd_key) as product_key,
+	pn.prd_id as product_id,
+	pn.prd_key as product_number,
+	pn.prd_nm as product_name,
+	pn.cat_id as category_id,
+	pc.cat as category,
+	pc.subcat as subcategory,
+	pc.maintenance,
+	pn.prd_cost as cost,
+	pn.prd_line as product_line,
+	pn.prd_start_dt as start_date
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where pn.prd_end_dt is null; -- filter all Historical data
+
+select * from gold.dim_products dp;
+-- -----------------------------------------------------------------------------------------------------
+-- creation of the Fact Sales View on the golden layer
+create view gold.fact_sales as
+select
+sd.sls_ord_num as order_number,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt as order_date,
+sd.sls_ship_dt as shipping_date,
+sd.sls_due_dt as due_date,
+sd.sls_sales as sale_amount,
+sd.sls_quantity as quantity,
+sd.sls_price as price
+from silver.crm_sales_details sd
+left join gold.dim_products pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers cu
+on sd.sls_cust_id = cu.customer_id;
+
+select * from gold.fact_sales fs;
+
+-- Foreign Key Integrity
+select *
+from gold.fact_sales f
+left join gold.dim_customers c
+on c.customer_key = f.customer_key
+left join gold.dim_products p
+on p.product_key = f.product_key
+where p.product_key is null;
